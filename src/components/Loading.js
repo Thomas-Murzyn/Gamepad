4573419e031c85498a80d69185736835e5c2e665
Thomas Murzyn <murzynthomas@gmail.com>
2023-02-15 16:45:33 +0100

    Add a waiting message on Loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,6 +3,11 @@ import ReactLoading from "react-loading";
 
 const Loading = ({ type, color }) => (
   <>
+    <h3 style={{ padding: "0px 20px", textAlign: "center" }}>
+      Ceci est un site de démo dont le serveur est déployé sur un hébergeur de
+      manière gratuite. Le service étant gratuit le serveur peut mettre une peu
+      plus de temps à charger.
+    </h3>
     <ReactLoading type={"bars"} color={color} height={70} width={100} />
     <h3 style={{ marginTop: "20px" }}>Powered by Rawg</h3>
   </>
